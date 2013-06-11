546ea2faa56838eadd379c2c63e66f49f1358a37
pigmelons <pigmelons@163.com>
2013-06-11 10:09:18 +0800

    fuck

--- TestForGit2/SecondViewController.m
+++ TestForGit2/SecondViewController.m
@@ -17,7 +17,8 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
+    if (self)
+    {
         self.title = NSLocalizedString(@"Second", @"Second");
         self.tabBarItem.image = [UIImage imageNamed:@"second"];
     }
